d17cc98b55526a61102b33ef626b6819103f8f30
snigenigmatic <kaustubhc.11052gmail.com>
2024-10-17 09:55:46 +0530

    reverted to the comments

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,11 +8,11 @@ const anton = Anton({ subsets: ['latin'], weight: ['400'] });
 export function Footer() {
   return (
     <footer className="bg-mainBlue text-lYellow py-6">
-      
+      {/* Top Line */}
       <div className="border-t-2 border-lYellow mb-4"></div>
 
       <div className="container px-4 mx-auto grid text-centre grid-cols-1 md:grid-cols-3 gap-4 items-center">
-        
+        {/* Left Section: ESUMMIT and Icons */}
         <div className="flex flex-col items-start">
           <h1 className={`${anton.className} text-4xl mb-2`}>ESUMMIT</h1>
 
@@ -27,7 +27,7 @@ export function Footer() {
           </div>
         </div>
 
-        
+        {/* Center Section: Contact Details */}
         <div className="flex flex-col items-start">
         <h2 className={`${anton.className} text-2xl mb-2`}>Contact Us</h2>
           <div className="flex items-center mb-2">
